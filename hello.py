25494568b09318c707504cc8c43a90ca113113ee
Alberto <alberto.martinez.martinez.1@gmail.com>
2020-09-04 13:29:07 +0200

    WIP: env variables

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
+import os
+
 from flask_restful import Resource
 
 
 class Hello(Resource):
     def get(self, name):
-        return {"Hello": name}
+        aux = os.getenv('TEST')
+        return {"Hello": name + aux}
