ee7bfbda1e2d33049455c5dbe58e6db4b9493eb0
AMU-013 <55637857+AMU-LDC@users.noreply.github.com>
2020-09-05 15:31:15 +0200

    Cloud SQL Connection

--- main.py
+++ main.py
@@ -1,21 +1,56 @@
+import os
+import pymysql
+
 from flask import Flask
-from flask_cors import CORS
 from flask_restful import Api
 
 from controller.appartment import ApartmentController
 from controller.location import LocationController
 from controller.recommender_restaurant import RecommenderRestaurantController
+
 from hello import Hello
+
+db_user = os.environ.get('CLOUD_SQL_USERNAME')
+db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
-CORS(app)
 api = Api(app)
 
-api.add_resource(Hello, '/hello/<name>')
+@app.route('/')
+
+def main():
+    # When deployed to App Engine, the `GAE_ENV` environment variable will be
+    # set to `standard`
+    if os.environ.get('GAE_ENV') == 'standard':
+        # If deployed, use the local socket interface for accessing Cloud SQL
+        unix_socket = '/cloudsql/{}'.format(db_connection_name)
+        cnx = pymysql.connect(user=db_user, password=db_password,
+                              unix_socket=unix_socket, db=db_name)
+    else:
+        # If running locally, use the TCP connections instead
+        # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
+        # so that your application can use 127.0.0.1:3306 to connect to your
+        # Cloud SQL instance
+        host = '127.0.0.1'
+        cnx = pymysql.connect(user=db_user, password=db_password,
+                              host=host, db=db_name)
+
+    with cnx.cursor() as cursor:
+        cursor.execute('SELECT * FROM airbnb')
+        result = cursor.fetchall()
+        current_msg = result[0][0]
+    cnx.close()
+
+    return str(current_msg)
+
+api.add_resource(Hello, 'Holiiiiiiiiii')
 api.add_resource(LocationController, '/locations')
-api.add_resource(ApartmentController, '/apartments/<neighborhood>/<pax>')
+api.add_resource(ApartmentController, '/apartments/<neighborhood>')
 api.add_resource(RecommenderRestaurantController, '/recommender/restaurant')
 
 if __name__ == '__main__':
