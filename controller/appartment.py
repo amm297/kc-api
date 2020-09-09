61e4b7e113b182f9b2cd0a59aec44fea23f30147
Alberto <alberto.martinez.martinez.1@gmail.com>
2020-09-09 21:07:30 +0200

    Functional version

--- controller/appartment.py
+++ controller/appartment.py
@@ -7,6 +7,6 @@ class ApartmentController(Resource):
     def get(self, neighborhood, pax):
         db = DBConnection()
         results = db.execute_query(
-            f'SELECT id, listing_url, name, description, house_rules, picture_url, property_type, room_type, accommodates, bathrooms, bedrooms, beds, bed_type, amenities, square_feet, street, neighbourhood, zipcode, latitude, longitude, price, security_deposit, cleaning_fee, cancellation_policy, number_of_reviews, review_scores_rating, review_scores_accuracy, review_scores_cleanliness, review_scores_checkin, review_scores_communication, review_scores_location, review_scores_value FROM airbnb WHERE neighbourhood = \'{neighborhood}\' AND accomodates=\'{pax}\' LIMIT 5')
+            f'SELECT id, listing_url, name, description, house_rules, picture_url, property_type, room_type, accommodates, bathrooms, bedrooms, beds, bed_type, amenities, square_feet, street, neighbourhood, zipcode, latitude, longitude, price, security_deposit, cleaning_fee, cancellation_policy, number_of_reviews, review_scores_rating, review_scores_accuracy, review_scores_cleanliness, review_scores_checkin, review_scores_communication, review_scores_location, review_scores_value, new_rate FROM airbnb WHERE neighbourhood = \'{neighborhood}\' AND accommodates=\'{pax}\' LIMIT 5')
 
         return {"apartments": results} if results else {}
